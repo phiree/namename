e27f92dd2a549933d1e695962052481bab81832a
phiree <phiree@gmail.com>
2012-05-31 23:52:02 +0800

    add ProInfoMap.cs

---
+++ NameName.Model/Mapping/ProInfoMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+namespace NameName.Model.Mapping
+{
+   public class ProInfoMap:ClassMap<ProInfo>
+    {
+       public ProInfoMap()
+       {
+           Id(x => x.ProID);
+           Map(x => x.DeleteFlag);
+           Map(x => x.Name);
+           Map(x => x.PicName);
+           Map(x => x.ProCate);
+           Map(x => x.Unit);
+           HasMany<ProPrice>(x => x.ProPrices);
+          
+
+       }
+    }
+}
---
+++ NameName.Model/Mapping/ProPriceMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+namespace NameName.Model.Mapping
+{
+   public class ProPriceMap:ClassMap<ProPrice>
+    {
+       public ProPriceMap()
+       {
+           Id(x => x.Id);
+           Map(x => x.Price);
+           References<ProInfo> (x => x.ProInfo);
+           References<AreaInfo>(x => x.AreaInfo);
+       }
+
+    }
+}
