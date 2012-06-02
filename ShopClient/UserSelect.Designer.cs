18fd77e3f17abf56d1d52681ea6f6ccd9467aa89
qiji <liu_liu66@sina.com>
2012-06-02 20:38:11 +0800

    userselect

--- ShopClient/UserSelect.Designer.cs
+++ ShopClient/UserSelect.Designer.cs
@@ -30,35 +30,84 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnSelect = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lbshopinfo = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(76, 64);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 94);
+            this.panel1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(616, 231);
+            this.panel1.Size = new System.Drawing.Size(813, 364);
             this.panel1.TabIndex = 0;
             // 
             // btnSelect
             // 
-            this.btnSelect.Location = new System.Drawing.Point(65, 13);
+            this.btnSelect.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelect.Location = new System.Drawing.Point(0, 0);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.btnSelect.Name = "btnSelect";
-            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.Size = new System.Drawing.Size(119, 94);
             this.btnSelect.TabIndex = 1;
-            this.btnSelect.Text = "门店选择";
+            this.btnSelect.Text = "门店";
             this.btnSelect.UseVisualStyleBackColor = true;
             this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lbshopinfo);
+            this.panel2.Controls.Add(this.btnExit);
+            this.panel2.Controls.Add(this.btnSelect);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(813, 94);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(692, 0);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(121, 94);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lbshopinfo
+            // 
+            this.lbshopinfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbshopinfo.Location = new System.Drawing.Point(119, 0);
+            this.lbshopinfo.Name = "lbshopinfo";
+            this.lbshopinfo.Size = new System.Drawing.Size(573, 94);
+            this.lbshopinfo.TabIndex = 4;
+            this.lbshopinfo.Text = "label1";
+            this.lbshopinfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // UserSelect
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(809, 456);
-            this.Controls.Add(this.btnSelect);
+            this.ClientSize = new System.Drawing.Size(813, 458);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.Name = "UserSelect";
-            this.Text = "UserSelect";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "人员选择";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UserSelect_FormClosed);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -67,6 +116,9 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lbshopinfo;
 
     }
 }
--- ShopClient/UserSelect.cs
+++ ShopClient/UserSelect.cs
@@ -17,28 +17,33 @@ namespace ShopClient
         Guid ShopId = GlobalValue.ShopID;
         public UserSelect()
         {
-            
+
             if (ShopId == null || ShopId == Guid.Empty)
             {
                 return;
             }
-          
+
             InitializeComponent();
-                       LoadUsers(); 
+            LoadUsers();
         }
+
         public void LoadUsers()
         {
+
             ShopInfo shop = new DALShopInfo().GetByShopID(ShopId);
+
+            lbshopinfo.Text = shop.AreaInfo.AreaName + " - " + shop.ShopName;
+
             IList<UserInfo> users = shop.ShopUsers;
-            Dictionary<string,UserInfo> SourceUsers=new Dictionary<string,UserInfo>();
-            foreach(UserInfo user in users)
+            Dictionary<string, UserInfo> SourceUsers = new Dictionary<string, UserInfo>();
+
+            foreach (UserInfo user in users)
             {
-             SourceUsers.Add(user.UserName,user);
+                SourceUsers.Add(user.UserName, user);
             }
 
-            GridBuilder<UserInfo> UserGrid = new GridBuilder<UserInfo>(SourceUsers,
-                new Size(100, 100), panel1, 4, 20, 20);
-         
+            GridBuilder<UserInfo> UserGrid = new GridBuilder<UserInfo>(SourceUsers, new Size(120, 120), panel1, 3, 50, 50);
+
             UserGrid.OnBindButtonClick += new GridBuilder<UserInfo>.BindButtonClick(UserGrid_OnBindButtonClick);
             UserGrid.BuildButtons();
         }
@@ -47,6 +52,7 @@ namespace ShopClient
         {
             b.Click += new EventHandler(UserBtnclick);
         }
+
         void UserBtnclick(object sender, EventArgs e)
         {
             Login login = new Login();
@@ -60,7 +66,7 @@ namespace ShopClient
             ToExit = false;
             this.Close();
             new ShopSelect().Show();
-            
+
         }
 
         private void UserSelect_FormClosed(object sender, FormClosedEventArgs e)
@@ -70,6 +76,11 @@ namespace ShopClient
                 Application.Exit();
             }
         }
-        
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
     }
 }
