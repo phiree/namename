2e31bdfa57c7acee4851b5c6e22a178016f7e83e
phiree <phiree@gmail.com>
2012-06-02 17:26:16 +0800

    database config

---
+++ ShopClient/ShopClientConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShopClient
+{
+  public  class ShopClientConfig
+    {
+     public const string Conn = "uid=tourdb;pwd=howwasyourdaytoday;database=zy_shop;server=data.tourzj.gov.cn,98;";
+    }
+}
