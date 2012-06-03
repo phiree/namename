ec4d9860e3176311f7ae1c19828be7b7f628ae91
qiji <liu_liu66@sina.com>
2012-06-03 12:06:00 +0800

    Duty判断

--- ShopClient/main.Designer.cs
+++ ShopClient/main.Designer.cs
@@ -32,15 +32,15 @@
             this.btnpwd = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnAsk = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnDutyBegin = new System.Windows.Forms.Button();
             this.btnDutyEnd = new System.Windows.Forms.Button();
+            this.btnDutyBegin = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,12 +49,12 @@
             this.panel1.Controls.Add(this.btnpwd);
             this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.button5);
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.btnCheck);
+            this.panel1.Controls.Add(this.btnAsk);
             this.panel1.Controls.Add(this.btnClose);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.btnNew);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btnDutyEnd);
             this.panel1.Controls.Add(this.btnDutyBegin);
@@ -95,25 +95,25 @@
             this.button5.Text = "入库单";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnCheck
             // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button4.Location = new System.Drawing.Point(457, 0);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 76);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "盘点";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnCheck.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnCheck.Location = new System.Drawing.Point(457, 0);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 76);
+            this.btnCheck.TabIndex = 7;
+            this.btnCheck.Text = "盘点";
+            this.btnCheck.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnAsk
             // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button3.Location = new System.Drawing.Point(382, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 76);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "要货单";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnAsk.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnAsk.Location = new System.Drawing.Point(382, 0);
+            this.btnAsk.Name = "btnAsk";
+            this.btnAsk.Size = new System.Drawing.Size(75, 76);
+            this.btnAsk.TabIndex = 6;
+            this.btnAsk.Text = "要货单";
+            this.btnAsk.UseVisualStyleBackColor = true;
             // 
             // btnClose
             // 
@@ -134,25 +134,25 @@
             this.label2.Size = new System.Drawing.Size(41, 76);
             this.label2.TabIndex = 11;
             // 
-            // button2
+            // btnBack
             // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button2.Location = new System.Drawing.Point(266, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 76);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "退货";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnBack.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnBack.Location = new System.Drawing.Point(266, 0);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 76);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "退货";
+            this.btnBack.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnNew
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button1.Location = new System.Drawing.Point(191, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 76);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "新开单据";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnNew.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnNew.Location = new System.Drawing.Point(191, 0);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 76);
+            this.btnNew.TabIndex = 4;
+            this.btnNew.Text = "新开单据";
+            this.btnNew.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -161,6 +161,16 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 76);
             this.label1.TabIndex = 10;
+            // 
+            // btnDutyEnd
+            // 
+            this.btnDutyEnd.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnDutyEnd.Location = new System.Drawing.Point(75, 0);
+            this.btnDutyEnd.Name = "btnDutyEnd";
+            this.btnDutyEnd.Size = new System.Drawing.Size(75, 76);
+            this.btnDutyEnd.TabIndex = 3;
+            this.btnDutyEnd.Text = "交班";
+            this.btnDutyEnd.UseVisualStyleBackColor = true;
             // 
             // btnDutyBegin
             // 
@@ -173,25 +183,15 @@
             this.btnDutyBegin.UseVisualStyleBackColor = true;
             this.btnDutyBegin.Click += new System.EventHandler(this.btnDutyBegin_Click);
             // 
-            // btnDutyEnd
-            // 
-            this.btnDutyEnd.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnDutyEnd.Location = new System.Drawing.Point(75, 0);
-            this.btnDutyEnd.Name = "btnDutyEnd";
-            this.btnDutyEnd.Size = new System.Drawing.Size(75, 76);
-            this.btnDutyEnd.TabIndex = 3;
-            this.btnDutyEnd.Text = "交班";
-            this.btnDutyEnd.UseVisualStyleBackColor = true;
-            // 
-            // Cash
+            // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(836, 511);
             this.Controls.Add(this.panel1);
-            this.Name = "Cash";
+            this.Name = "main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "门店收银";
+            this.Text = "门店系统";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Cash_Load);
             this.panel1.ResumeLayout(false);
@@ -206,11 +206,11 @@
         private System.Windows.Forms.Button btnpwd;
         private System.Windows.Forms.Button btnDutyEnd;
         private System.Windows.Forms.Button btnDutyBegin;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnAsk;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
--- ShopClient/main.cs
+++ ShopClient/main.cs
@@ -13,6 +13,10 @@ namespace ShopClient
 {
     public partial class main : Form
     {
+
+        DALDuty dalduty = new DALDuty();
+        Shop_DutyInfo dutyinfo;
+
         public main()
         {
             ClientLogin();
@@ -74,13 +78,39 @@ namespace ShopClient
         {
             this.Show();
             this.Text = GlobalValue.GShop.AreaInfo.AreaName + "-" + GlobalValue.GShop.ShopName + "-" + GlobalValue.GUser.TrueName + " 正在使用 么么 门店系统";
+            //不可能为null的！
+            GlobalValue.GAccount = new DALAccount().GetCurrAccount();
+
             //创建界面了！！！
+            string errorstr;
+            Shop_DutyInfo sd = dalduty.OnDuty(GlobalValue.GUser, GlobalValue.GShop, GlobalValue.GAccount, false, out errorstr);
 
-            //判断当前的当班信息,如果当前用户在当前门店当班，则允许，
-            //如果当前门店不是当前用户当班，则不允许，
-            //如果当前用户
+            if (sd != null)
+            {
+                //正在当班
+                dutyinfo = sd;
+                //开始当班
+                btnDutyBegin.Enabled = false;
+                btnDutyEnd.Enabled = true;
+                btnNew.Enabled = true;
+                btnBack.Enabled = true;
+                btnAsk.Enabled = false;
+                btnCheck.Enabled = false;
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(errorstr))
+                {
+                    //允许当班
+                    btnDutyBegin.Enabled = true;
 
-
+                    btnDutyEnd.Enabled = false;
+                    btnNew.Enabled = false;
+                    btnBack.Enabled = false;
+                    btnAsk.Enabled = true;
+                    btnCheck.Enabled = true;
+                }
+            }
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -95,6 +125,23 @@ namespace ShopClient
 
         private void btnDutyBegin_Click(object sender, EventArgs e)
         {
+            string errorstr;
+            Shop_DutyInfo sd = dalduty.OnDuty(GlobalValue.GUser, GlobalValue.GShop, GlobalValue.GAccount, true, out errorstr);
+            if (sd != null)
+            {
+                dutyinfo = sd;
+                //开始当班
+                btnDutyBegin.Enabled = false;
+                btnDutyEnd.Enabled = true;
+                btnNew.Enabled = true;
+                btnBack.Enabled = true;
+                btnAsk.Enabled = false;
+                btnCheck.Enabled = false;
+            }
+            else
+            {
+                GlobalFun.MessageBoxError(errorstr);
+            }
 
         }
     }
