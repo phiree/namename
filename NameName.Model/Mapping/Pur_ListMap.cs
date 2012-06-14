3f938de49dfc731ac9214ebe1476e0e4e30f8a43
phiree <phiree@gmail.com>
2012-06-14 10:37:08 +0800

    Cai Gou Dan

---
+++ NameName.Model/Mapping/Pur_ListMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+namespace NameName.Model.Mapping
+{
+    public class Pur_ListMap : ClassMap<Pur_List>
+    {
+        public Pur_ListMap()
+        {
+            Id(x => x.PurBillNo);
+            References<AreaInfo>(x => x.AreaInfo);
+            Map(x => x.ChkDate);
+            References<UserInfo>(x => x.ChkUser);
+            References<UserInfo>(x => x.PurUser);
+            Map(x => x.State);
+        }
+    }
+}
