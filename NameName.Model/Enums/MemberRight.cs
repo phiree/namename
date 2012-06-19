70a81df72acc95f749839b5d06b884c3de679de2
phiree <phiree@gmail.com>
2012-06-20 05:01:55 +0800

    添加枚举

---
+++ NameName.Model/Enums/MemberRight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NameName.Model.Enums
+{
+   public enum PurchaseState
+    {
+      未审核=0
+       ,已审核=1
+    }
+}
---
+++ NameName.Model/Enums/PurchaseState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NameName.Model.Enums
+{
+   public enum MemberRight
+    {
+       总经理=1<<1
+       ,财务=1<<2
+       ,采购=1<<3
+    }
+}
